8436522370fa649cf18e2f698eff278722934e0f
Malin Boëthius <malinboethius@Malins-iMac.local>
2022-04-21 20:54:52 +0200

    :beers:

--- app.js
+++ app.js
@@ -6,33 +6,34 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-const mongoose = require('mongoose');
-require('dotenv/config');
+//const mongoose = require('mongoose');
+//require('dotenv/config');
 
 var app = express();
 
-//const MongoClient = require("mongodb").MongoClient;
+const MongoClient = require("mongodb").MongoClient;
 
-//MongoClient.connect("mongodb://127.0.0.1:27017", {
+MongoClient.connect("mongodb://127.0.0.1:27017", {
 //MongoClient.connect("mongodb+srv://gdtUser:<gdtUserPassword>@cluster0.fjhzd.mongodb.net/users?retryWrites=true&w=majority", {
-   // useUnifiedTopology: true
-//})
-//.then(client => {
-  //  console.log("vi är uppkopplade mot databasen");
-    //const db = client.db("users");
-    //app.locals.db = db;
+    useUnifiedTopology: true
+})
+.then(client => {
+   console.log("vi är uppkopplade mot databasen");
+    const db = client.db("users");
+    app.locals.db = db;
 
-//})
+})
 
 
 //CONECT TO DB
-mongoose.connect(
-//  process.env.DB_CONNECTION,
- // {useNewUrlParse: true },
-  'mongodb+srv://gdtUser:gdtUserPassword@cluster0.fjhzd.mongodb.net/users?retryWrites=true&w=majority', 
-  () => 
-console.log('connected to db')
-);
+//mongoose.connect(
+ process.env.DB_CONNECTION,
+ {useNewUrlParse: true },
+
+  //'mongodb+srv://gdtUser:gdtUserPassword@cluster0.fjhzd.mongodb.net/users?retryWrites=true&w=majority', 
+ // () => 
+//console.log('connected to db')
+//);
 
 
 app.use(logger('dev'));
